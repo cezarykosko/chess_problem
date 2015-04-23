abbca76c42b2cf37cdb1c741ff6d1f891497e68c
Cezary Kosko <cezary@ytrre.com>
2015-04-23 21:43:14 +0200

    changed the logic of King.beats to include diagonals

--- src/main/scala/Piece.scala
+++ src/main/scala/Piece.scala
@@ -1,4 +1,4 @@
-sealed abstract class Piece(protected val coords: (Int, Int)) {
+sealed abstract class Piece(var coords: (Int, Int)) {
 
   // NOTE: assuming a pawn beats its coords
   def beats(coords: (Int, Int)): Boolean
@@ -18,25 +18,25 @@ sealed abstract class Piece(protected val coords: (Int, Int)) {
     distHorizontal(coords) == distVertical(coords)
 }
 
-case class King(override val coords: (Int, Int)) extends Piece(coords) {
+case class King extends Piece(0,0) {
   override def beats(coords: (Int, Int)): Boolean =
-    this.distHorizontal(coords) + this.distVertical(coords) <= 1
+    Math.max(this.distHorizontal(coords),this.distVertical(coords)) <= 1
 }
 
-case class Queen(override val coords: (Int, Int)) extends Piece(coords) {
+case class Queen extends Piece(0,0) {
   override def beats(coords: (Int, Int)): Boolean =
     diagonalBeats(coords) || straightBeats(coords)
 }
 
-case class Bishop(override val coords: (Int, Int)) extends Piece(coords) {
+case class Bishop extends Piece(0,0) {
   override def beats(coords: (Int, Int)) = diagonalBeats(coords)
 }
 
-case class Rook(override val coords: (Int, Int)) extends Piece(coords) {
+case class Rook extends Piece(0,0) {
   override def beats(coords: (Int, Int)) = straightBeats(coords)
 }
 
-case class Horse(override val coords: (Int, Int)) extends Piece(coords) {
+case class Knight extends Piece(0,0) {
   override def beats(coords: (Int, Int)) = {
     val dists = (this.distHorizontal(coords), this.distVertical(coords))
     dists ==(0, 0) || dists ==(2, 1) || dists ==(1, 2)

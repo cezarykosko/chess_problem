22eb1769fe5ac37cf9c68e75956dd077049958ab
Cezary Kosko <cezary@ytrre.com>
2015-04-23 23:54:57 +0200

    removed pawn wrapper

--- src/main/scala/ChessProblem.scala
+++ src/main/scala/ChessProblem.scala
@@ -1,5 +1,7 @@
 object ChessProblem {
 
+  type Piece = (Int, Int, Int)
+
   final private val KINGS = 0
   final private val QUEENS = 1
   final private val BISHOPS = 2
@@ -24,16 +26,42 @@ object ChessProblem {
 
     val count = kings + queens + bishops + rooks + knights
 
-    val result = backtrack(chessboard, Array(kings, queens, bishops, rooks, knights), count, List())//new PieceState(kings, queens, bishops, rooks, knights))
+    val result = backtrack(chessboard, Array(kings, queens, bishops, rooks, knights), count, List())
     println(result)
   }
 
   //TODO: rename
   private def isAfter(piece1: Piece, piece2: Piece): Boolean =
-    piece1.getClass == piece2.getClass && isAfter(piece1.coords, piece2.coords)
+    piece1._3 == piece2._3 && (piece1._1 > piece2._1 || (piece1._1 == piece2._1 && piece1._2 > piece2._2))
 
-  private def isAfter(coord1: (Int, Int), coord2: (Int, Int)): Boolean =
-    coord1._1 > coord2._1 || (coord1._1 == coord2._1 && coord1._2 > coord2._2)
+  private final val abs = Math.abs(_: Int)
+
+  protected val distHorizontal = (c1: (Int, Int), c2: (Int, Int)) => abs(c1._1 - c2._1)
+
+  protected val distVertical = (c1: (Int, Int), c2: (Int, Int)) => abs(c1._2 - c2._2)
+
+  protected val straightBeats = (c1: (Int, Int), c2: (Int, Int)) =>
+    distHorizontal(c1, c2) == 0 || distVertical(c1, c2) == 0
+
+  protected val diagonalBeats = (c1: (Int, Int), c2: (Int, Int)) =>
+    distHorizontal(c1, c2) == distVertical(c1, c2)
+
+  private def beats(piece1: Piece, piece2: Piece): Boolean =
+    beats(piece1, (piece2._1, piece2._2))
+
+  private def beats(piece: Piece, coords: (Int, Int)): Boolean = {
+    val pCoords = (piece._1, piece._2)
+    piece._3 match {
+      case KINGS => Math.max(distHorizontal(pCoords, coords), distVertical(pCoords, coords)) <= 1
+      case QUEENS => diagonalBeats(pCoords, coords) || straightBeats(pCoords, coords)
+      case BISHOPS => diagonalBeats(pCoords, coords)
+      case ROOKS => straightBeats(pCoords, coords)
+      case KNIGHTS =>
+        val dists = (distHorizontal(pCoords, coords), distVertical(pCoords, coords))
+        dists ==(0, 0) || dists ==(2, 1) || dists ==(1, 2)
+      case default => false
+    }
+  }
 
   private def remove(n: Int) = (m: Array[Int]) => {
     val nArr = m.clone()
@@ -41,40 +69,40 @@ object ChessProblem {
     nArr
   }
 
-  private def place_piece(fields: Seq[(Int, Int)], piecesLeft: Array[Int], sparePiecesCount: Int, piecesOnTheBoard: List[Piece], newPiece: Piece): Int = {
+  private def place_piece(fields: Seq[(Int, Int)], piecesLeft: Array[Int], sparePiecesCount: Int, piecesOnTheBoard: List[Piece], pieceType: Int): Int = {
     (for {
       coords <- fields
     } yield {
-        newPiece.coords = coords
-        if (piecesOnTheBoard exists ((p: Piece) => newPiece.beats(p) || isAfter(newPiece, p)))
+        val newPiece = (coords._1, coords._2, pieceType)
+
+        if (piecesOnTheBoard exists ((p: Piece) => isAfter(newPiece, p) || beats(newPiece, p))) // || isAfter(newPiece, p)))
           0
         else
-          backtrack(fields filter (!newPiece.beats(_)), piecesLeft, sparePiecesCount, newPiece :: piecesOnTheBoard)
+          backtrack(fields filter (!beats(newPiece, _)), piecesLeft, sparePiecesCount, newPiece :: piecesOnTheBoard)
       }).sum
   }
 
   // assuming there's available fields & pieces
   private def backtrack_step(fields: Seq[(Int, Int)], piecesLeft: Array[Int], sparePiecesCount: Int, piecesOnTheBoard: List[Piece]): Int = {
     if (piecesLeft(QUEENS) != 0)
-      place_piece(fields, remove(QUEENS)(piecesLeft), sparePiecesCount - 1, piecesOnTheBoard, new Queen)
+      place_piece(fields, remove(QUEENS)(piecesLeft), sparePiecesCount - 1, piecesOnTheBoard, QUEENS)
     else if (piecesLeft(ROOKS) != 0)
-      place_piece(fields, remove(ROOKS)(piecesLeft), sparePiecesCount - 1, piecesOnTheBoard, new Rook)
+      place_piece(fields, remove(ROOKS)(piecesLeft), sparePiecesCount - 1, piecesOnTheBoard, ROOKS)
     else if (piecesLeft(BISHOPS) != 0)
-      place_piece(fields, remove(BISHOPS)(piecesLeft), sparePiecesCount - 1, piecesOnTheBoard, new Bishop)
+      place_piece(fields, remove(BISHOPS)(piecesLeft), sparePiecesCount - 1, piecesOnTheBoard, BISHOPS)
     else if (piecesLeft(KINGS) != 0)
-      place_piece(fields, remove(KINGS)(piecesLeft), sparePiecesCount - 1, piecesOnTheBoard, new King)
+      place_piece(fields, remove(KINGS)(piecesLeft), sparePiecesCount - 1, piecesOnTheBoard, KINGS)
     else if (piecesLeft(KNIGHTS) != 0)
-      place_piece(fields, remove(KNIGHTS)(piecesLeft), sparePiecesCount - 1, piecesOnTheBoard, new Knight)
+      place_piece(fields, remove(KNIGHTS)(piecesLeft), sparePiecesCount - 1, piecesOnTheBoard, KNIGHTS)
     else
       1
   }
 
   private def backtrack(fields: Seq[(Int, Int)], piecesLeft: Array[Int], sparePiecesCount: Int, piecesOnTheBoard: List[Piece]): Int = {
-    //println(fields)
     if (sparePiecesCount == 0)
       1
-    else if (fields.isEmpty)
-      0
+    //else if (fields.isEmpty)
+    //  0
     else backtrack_step(fields, piecesLeft, sparePiecesCount, piecesOnTheBoard)
   }
 }
